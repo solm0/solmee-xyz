b833eb100a9a5b79501cbbd5675db373808a1456
정솔미 <solmi-@kookmin.ac.kr>
2024-11-02 22:30:16 +0900

    wikilink to markdown plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import wikiLink from 'remark-wiki-link';
 
-// https://astro.build/config
-export default defineConfig({});
+const pageUrlPathPrefix = 'markdowns/';
+
+export default defineConfig({
+    markdown: {
+      remarkPlugins: [
+        [
+          wikiLink,
+          {
+            hrefTemplate: (permalink) => `/${pageUrlPathPrefix}${permalink}`,
+            pageResolver: (name) => [name.replace(/ /g, '-').toLowerCase()],
+          },
+        ],
+      ],
+    },
+  });
