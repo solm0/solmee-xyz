f1b3e1dede236053d3bd4bf964085b0fea8e21a1
정솔미 <solmi-@kookmin.ac.kr>
2024-11-06 23:01:48 +0900

    vercel error

--- src/scripts/loadNotes.js
+++ src/scripts/loadNotes.js
@@ -1,12 +1,9 @@
-// src/scripts/loadNotes.js
-
 export function loadNotes() {
     const allNotes = import.meta.glob('../pages/markdowns/*.md', { eager: true });
 
     return Object.keys(allNotes).map((path) => {
         const note = allNotes[path];
         
-        // Transform the path into a URL-friendly structure
         const url = path
             .replace('../pages/markdowns', '/markdowns')
             .replace('.md', '');
