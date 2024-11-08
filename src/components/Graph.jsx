0b2617ef7fc9a6c1b89cc628c2627c1344aa676c
정솔미 <solmi-@kookmin.ac.kr>
2024-11-08 23:10:59 +0900

    node link to note

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,19 +1,26 @@
 import ForceGraph2D from 'react-force-graph-2d';
-import myData from '../data/graphData.json'
+import graphData from '../data/graphData.json'
 
 function MyGraphComponent() {
+  const handleNodeClick = (node) => {
+    const noteUrl = `/markdowns/${node.id}`;
+    window.location.href = noteUrl;
+  };
 
   return (
     <ForceGraph2D
-      graphData={myData}
+      graphData={graphData}
       width={800}
       height={600}
       backgroundColor='#e4e9e1'
-      nodeVal={(node) => node.val}
-      nodeLabel={() => null}
       minZoom={1}
       maxZoom={4}
+
       linkWidth={1}
+
+      nodeVal={(node) => node.val}
+      nodeLabel={() => null}
+      onNodeClick={handleNodeClick}
       nodeCanvasObject={(node, ctx, globalScale) => {
 
         const size = Math.sqrt(node.val) * 5;
@@ -24,10 +31,10 @@ function MyGraphComponent() {
 
         const fontSize = 12 / globalScale;
         ctx.font = `${fontSize}px Sans-Serif`;
-        ctx.fillStyle = 'transparent';
+        ctx.fillStyle = 'grey';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
-        ctx.fillText(node.name, node.x, node.y + size + fontSize); // Adjust Y for center-align below node
+        ctx.fillText(node.name, node.x, node.y + size + fontSize);
       }}
     />
   );
