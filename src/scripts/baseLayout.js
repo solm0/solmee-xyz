e9ae0bada355fd8ffa487ac5f9854d97818e9bb2
정솔미 <solmi-@kookmin.ac.kr>
2024-11-09 16:05:48 +0900

    delete header

--- src/scripts/baseLayout.js
+++ src/scripts/baseLayout.js
@@ -1,25 +1,3 @@
-/* scroll listener for header nav */
-const body = document.body;
-const scrollUp = "scroll-up";
-const scrollDown = "scroll-down";
-let lastScroll = 0;
-
-window.addEventListener("scroll", () => {
-    const currentScroll = window.scrollY;
-    if (currentScroll <= 0) {
-        body.classList.remove(scrollUp);
-        return;
-    }
-    if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-        body.classList.remove(scrollUp);
-        body.classList.add(scrollDown);
-    } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
-        body.classList.remove(scrollDown);
-        body.classList.add(scrollUp);
-    }
-    lastScroll = currentScroll;
-});
-
 /* nav current location highlighting */
 document.addEventListener("DOMContentLoaded", () => {
     const navLinks = document.querySelectorAll('.nav-links a');
