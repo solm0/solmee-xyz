849978ae3a9f1ed4ff8b8a5ee73beda8adfccb7b
정솔미 <solmi-@kookmin.ac.kr>
2024-11-15 13:38:52 +0900

    graph color change

--- src/scripts/graphStyle.js
+++ src/scripts/graphStyle.js
@@ -6,12 +6,41 @@ const calcResponsiveFontSize = () => {
   return clamp(min, preferred, max);
 };
 
-export const GRAPHSTYLE = {
-  fontSize: calcResponsiveFontSize(),
-  backgroundColor: '#f8fbf8',
-  grayColor1: '#e4e9e1',
-  grayColor2: '#d7d7cd',
-  grayColor3: '#aaaaa0',
-  grayColor4: '#686868',
-  textColor: '#212121',
+const getGraphStyle = () => {
+  const root = document.documentElement;
+  const isDark = root.classList.contains('dark');
+
+  if (isDark) {
+    return {
+      fontSize: calcResponsiveFontSize(),
+      backgroundColor: '#1d1f21',
+      grayColor1: '#3c3e3b',
+      grayColor2: '#52524e',
+      grayColor3: '#7e7e76',
+      grayColor4: '#a4a4a4',
+      textColor: '#c9d3c9',
+    };
+  } else {
+    return {
+      fontSize: calcResponsiveFontSize(),
+      backgroundColor: '#f8fbf8',
+      grayColor1: '#e4e9e1',
+      grayColor2: '#d7d7cd',
+      grayColor3: '#aaaaa0',
+      grayColor4: '#686868',
+      textColor: '#212121',
+    };
+  }
 };
+
+export let GRAPHSTYLE = getGraphStyle();
+
+const handleThemeChange = () => {
+  GRAPHSTYLE = getGraphStyle();
+};
+
+const observer = new MutationObserver(() => {
+  handleThemeChange();
+});
+
+observer.observe(document.documentElement, { attributes: true, attributeFilter: ['class'] });
