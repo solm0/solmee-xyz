07571231a0530c9438f2e22fb4c80aa3215b2074
정솔미 <solmi-@kookmin.ac.kr>
2024-11-29 23:21:10 +0900

    rss error

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,25 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
+  const markdownFiles = import.meta.glob('./**/*.md');
+
+  const items = await pagesGlobToRssItems(markdownFiles, {
+    mapFrontmatter(frontmatter, filePath) {
+      const filename = filePath.split('/').pop().replace('.md', '');
+      return {
+        title: frontmatter.alias ? frontmatter.alias : 'Untitled',
+        description: frontmatter.description || '',
+        pubDate: frontmatter.date,
+        link: `/markdowns/${filename}`,
+      };
+    },
+  });
+
   return rss({
     title: 'solmee.xyz',
     description: 'Digital garden of Solmee',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items,
     customData: `<language>ko</language>`,
   });
 }
